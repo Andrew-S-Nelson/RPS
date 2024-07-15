ced1ff4b17a7fa5c344c714d3b38e10694d8a782
Andrew-S-Nelson <andrewnelson2596@gmail.com>
2024-07-15 09:25:48 -0400

    Create RPS.py

---
+++ RPS.py
@@ -0,0 +1,29 @@
+import random
+
+cpu = 0
+usr = 0
+
+while (1):
+    # prints title
+    print("Rock Paper Scissors")
+    print("cpu " + str(cpu) + " | usr " + str(usr))
+    
+    # User Plays
+    usrin = ""
+    print("User chooses ", sep="")
+    input(userin)
+    userin = userin.toupper()
+    if (userin == )
+    
+    # Computer Plays
+    cpurand = random.randint(0, 2)
+    if (cpurand == 0):
+        print('Computer choose rock')
+    elif (cpurand == 1):
+        print('Computer choose paper')
+    elif (cpurand == 2):
+        print ('Computer choose scissors')
+        
+    # Scoring
+    
+    print(cpurand)
