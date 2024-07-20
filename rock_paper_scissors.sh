5b491f230a9a9e40bb0bafca1ab2cf9402daf89c
Andrew-S-Nelson <andrewnelson2596@gmail.com>
2024-07-19 21:53:35 -0400

    Update rock_paper_scissors.sh

--- rock_paper_scissors.sh
+++ rock_paper_scissors.sh
@@ -1,45 +1,45 @@
 usr=0
 cpu=0
-
+#
 declare -a options
 options[0]=rock
 options[1]=paper
 options[2]=scissors
-
+#
 while [ 0 -eq 0 ]
 do
     echo -------------------
     echo rock paper scissors
     echo "usr $usr | cpu $cpu"
-    userin=""
+    userin=rock
     read -p 'user chooses: ' userin
     userchoice=0
-    
+    #
     # userin to numeric choice
-    if [ $userin == 'rock' ]
+    if [ "$userin" == 'rock' ]
     then
         userchoice=0
-    elif [ $userin == 'paper' ]
+    elif [ "$userin" == 'paper' ]
     then
         userchoice=1
-    elif [ $userin == 'scissors' ]
+    elif [ "$userin" == 'scissors' ]
     then
         userchoice=2
     else
         echo 'invalid input'
         continue
     fi
-    
+    #
     # computer choice
     cpuchoice=$(date +%N)
     cpuchoice=$[ ${cpuchoice:8} % 3 ]
     echo "cpu choose ${options[$cpuchoice]}"
-    
+    #
     # scoring
     if [ $cpuchoice == $userchoice ]
     then
         echo 'game is draw'
-    
+    #
     # user chooses rock
     elif [ $userchoice -eq 0 ]
     then
@@ -51,7 +51,7 @@ do
             echo 'user won'
             usr=$[ $usr + 1 ]
         fi
-        
+    #    
     # user chooses paper
     elif [ $userchoice -eq 1 ] 
     then
@@ -63,7 +63,7 @@ do
             echo 'usr won'
             usr=$[ $usr + 1 ]
         fi
-        
+    #    
     # user chooses scissors
     elif [ $userchoice -eq 2 ] 
     then
@@ -75,6 +75,6 @@ do
             echo 'usr won'
             usr=$[ $usr + 1 ]
         fi        
-        
+    #    
     fi
 done
